6f61b4bb7d52bcfc5214479653f695952bf004b2
Droidem <eplata05@gmail.com>
2023-06-22 20:15:22 -0500

    Updated code overall

---
+++ DroidAOE/Addons/Droid_Stack/config.cpp
@@ -0,0 +1,307 @@
+class CfgPatches {
+    class Droid_Stack {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 0.1;
+        requiredAddons[] = {
+            "DZ_Weapons_Ammunition",
+            "DZ_Weapons_Magazines"
+        };
+        magazines[] = {};
+    };
+};
+class CfgMagazines {
+    class Magazine_Base;
+    class Ammunition_Base: Magazine_Base {};
+    class Ammo_12Nail: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_12Stone: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_12gaPellets: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_12gaRubberSlug: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_12gaSlug: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_22: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_300: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_300AP: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_308AP: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_308APTracer: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_308Win: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_308WinTracer: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_338LP_FOX: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_357: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_380: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_44MAGFOX: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_458Chey: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_45ACP: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_45AP: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_50AEFOX: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_545x39: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_545x39Tracer: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_556x45: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_556x45Tracer: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_57x28: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_57x28AP: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_6x35KAC: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_762x25fox: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_762x39: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_762x39Tracer: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_762x54: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_762x54Tracer: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_792x33fox: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_792x57FOX: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_93x64: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_9x18: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_9x18_exp: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_9x19: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_9x21EXP: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_9x21FOX: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_9x39: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_9x39AP: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_AACBLK: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_AE127x99: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_AE300: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_AE338: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_AE408: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_AE46x30: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_AE46x30AP: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_AE50AE: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_AE57x28: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_AE57x28AP: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_AE762x25: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_AE792x33: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_AE792x57: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_BO_127x55STs: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_BO_145x114: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_BO_3006: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_BO_65C: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_BO_86B: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_Bleed: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_ER7RFW: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_Greed: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_Grudge: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_HuntingBolt: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_ImprovisedBolt_1: Ammunition_Base {
+        count = 100;
+    };
+    class Ammo_ImprovisedBolt_2: Ammunition_Base {
+        count = 100;
+    };
+    class BAW_Ammo_50BMG: Ammunition_Base {
+        count = 100;
+    };
+    class SNAFU_Ammo_127x55mm: Ammunition_Base {
+        count = 100;
+    };
+    class SNAFU_Ammo_127x55mmAP: Ammunition_Base {
+        count = 100;
+    };
+    class SNAFU_Ammo_145Cal: Ammunition_Base {
+        count = 100;
+    };
+    class SNAFU_Ammo_300ACC: Ammunition_Base {
+        count = 100;
+    };
+    class SNAFU_Ammo_338: Ammunition_Base {
+        count = 100;
+    };
+    class SNAFU_Ammo_44: Ammunition_Base {
+        count = 100;
+    };
+    class SNAFU_Ammo_45_70mm: Ammunition_Base {
+        count = 100;
+    };
+    class SNAFU_Ammo_46x30: Ammunition_Base {
+        count = 100;
+    };
+    class SNAFU_Ammo_500_SW: Ammunition_Base {
+        count = 100;
+    };
+    class SNAFU_Ammo_5_7x28mm: Ammunition_Base {
+        count = 100;
+    };
+    class SNAFU_Ammo_762x25: Ammunition_Base {
+        count = 100;
+    };
+    class SNAFU_MD_Ammo_556x45mm_M855A1: Ammunition_Base {
+        count = 100;
+    };
+    class TF_Ammo_20mm: Ammunition_Base {
+        count = 100;
+    };
+    class TF_Ammo_20mm_HE: Ammunition_Base {
+        count = 100;
+    };
+    class TF_Ammo_338LM: Ammunition_Base {
+        count = 100;
+    };
+    class TF_Ammo_57: Ammunition_Base {
+        count = 100;
+    };
+    class TTC_Ammo_277: Ammunition_Base {
+        count = 100;
+    };
+    class TTC_Ammo_3006: Ammunition_Base {
+        count = 100;
+    };
+    class TTC_Ammo_300blk: Ammunition_Base {
+        count = 100;
+    };
+    class TTC_Ammo_303: Ammunition_Base {
+        count = 100;
+    };
+    class TTC_Ammo_338: Ammunition_Base {
+        count = 100;
+    };
+    class TTC_Ammo_44Mag: Ammunition_Base {
+        count = 100;
+    };
+    class TTC_Ammo_4570: Ammunition_Base {
+        count = 100;
+    };
+    class TTC_Ammo_4630: Ammunition_Base {
+        count = 100;
+    };
+    class TTC_Ammo_50AE: Ammunition_Base {
+        count = 100;
+    };
+    class TTC_Ammo_50BMG: Ammunition_Base {
+        count = 100;
+    };
+    class TTC_Ammo_50Beo: Ammunition_Base {
+        count = 100;
+    };
+    class TTC_Ammo_792x33: Ammunition_Base {
+        count = 100;
+    };
+    class TTC_Ammo_8mm: Ammunition_Base {
+        count = 100;
+    };
+};
